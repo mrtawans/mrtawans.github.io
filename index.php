fd2edca8eeafd858c1b853484396148c665ddcae
Tawanwong <tawanwong@softcontrol.net>
2015-07-20 13:40:44 +0700

    update site

---
+++ index.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Mr.Tawan, A Web Developer, Freelancer & Traveller</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel='shortcut icon' href='assets/images/favicon.ico'>
+
+		<!-- DEFAULT -->
+		<link rel="stylesheet" href="assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/node_modules/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/node_modules/normalize.css/normalize.css">
+
+		<!-- CUSTOM -->
+		<link rel="stylesheet" href="assets/css/main.css">
+	</head>
+	<body>
+
+		<div class="container" id="header-introduction">
+
+			<div class="row">
+				<div class="col-sm-9 col-md-9 col-lg-9">
+					<div class="jumbotron-introduction">
+						<h1>TAWAN chotikanchinda</h1>
+						<p class="lead">
+							A both front & back-end Web Developer based in <span class="spc">Bangkok, Thailand</span>.<br />
+							I specialize in launching a clean website, usable functionality and reliable. <br />
+							I'm an easy to work with, friendly guy. You can find out more <span class="spc">about me</span> <br />
+							and then <span class="spc">get in touch</span> for your projects or just <span class="spc">say hi !</span><br />
+							<br />
+							I've learning Web Development since 16 years old. Totally <span class="spc">2,666</span> days already.
+						</p>
+						<div class="container">
+							<ul class="social list-inline">
+								<a href="http://www.facebook.com/tawan.chotikanchinda"><li class="fa fa-facebook fa-1x"></li></a>
+								<a href="http://www.twitter.com/mrtawans"><li class="fa fa-twitter fa-1x"></li></a>
+								<a href="http://www.instagram.com/mrtawans"><li class="fa fa-instagram fa-1x"></li></a>
+								<a href="http://www.github.com/mrtawans"><li class="fa fa-github fa-1x"></li></a>
+								<li class="text-email">me@mrtawan.com</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3 col-md-3 col-lg-3">
+					<div class="jumbotron-introduction-profile">
+						<img src="assets/images/mrtawan2.png" class="img-responsive img-circle">
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<hr class="introduction" />
+		<section id="content">
+
+		</section>
+		 
+		<footer>
+			<hr class="footer" />
+			<div class="container text-right">
+				<div class="pull-right">
+					<ul class="legals list-inline">
+						<li><a href="#">Personal Privacy</a></li>
+						<li><a href="#">Terms of Conditions</a></li>
+					</ul>
+					<p>Builded with HTML5 & Bootstrap by <a href="http://www.diinspire.com">DiInspire.com</a></p>
+				</div>
+			</div>
+		</footer>
+		<script type="text/javascript" src="assets/node_modules/jquery/dist/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+	</body>
+</html>
